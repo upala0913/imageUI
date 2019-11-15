8e93272be81c60500a37d711cc02e4e76ff26135
upala <upala178421@163.com>
2019-11-15 13:47:37 +0800

    分离头部与尾部页面

--- src/pages/personal.vue
+++ src/pages/personal.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="container" >
+        <image-header/>
         个人中心
     </div>
 </template>
 
 <script>
+    import imageHeader from '@/components/common/imageHeader'
     export default {
-        name: "personal"
+        name: "personal",
+        components: {
+            imageHeader
+        }
     }
 </script>
 
