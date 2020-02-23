273c065a03b1c42cfeffc05a9d88d69a3d62c7be
upala <upala178421@163.com>
2020-02-24 04:44:21 +0800

    factor：开发图片管理分页信息

--- src/pages/image/spaceSetting.vue
+++ src/pages/image/spaceSetting.vue
@@ -3,15 +3,15 @@
         <div class="space-title" >空间设置</div>
         <hr class="space-hr"/>
         <div class="image-space" >
-            <el-table :data="documentSpaces" border style="width: 100%">
-                <el-table-column prop="id" label="标识码" width="200"/>
-                <el-table-column prop="name" label="名称" width="120"/>
-                <el-table-column prop="index" label="目录index" width="100"/>
-                <el-table-column prop="type" label="类型" width="120"/>
-                <el-table-column sortable prop="createTime" label="创建时间" width="200"/>
-                <el-table-column prop="delFlag" label="删除标志" width="120"/>
-                <el-table-column prop="delFlagType" label="删除标志类型" width="120"/>
-                <el-table-column fixed="right" label="操作" width="130">
+            <el-table :data="documentSpaces" :stripe="true" :highlight-current-row="true" border style="width:100%">
+                <el-table-column fixed align="center" prop="id" label="标识码" width="200"/>
+                <el-table-column align="center" prop="name" label="名称" width="120"/>
+                <el-table-column align="center" prop="index" label="目录index" width="100"/>
+                <el-table-column align="center" prop="type" label="类型" width="120"/>
+                <el-table-column align="center" sortable prop="createTime" label="创建时间" width="200"/>
+                <el-table-column align="center" prop="delFlag" label="删除标志" width="120"/>
+                <el-table-column align="center" prop="delFlagType" label="删除标志类型" width="200"/>
+                <el-table-column fixed="right" align="center" label="操作" width="130">
                     <template slot-scope="scope">
                         <el-button @click="handleEdit(scope.row)" type="text" size="small">
                             <i class="el-icon-edit"/>&nbsp;编辑
@@ -41,11 +41,11 @@
                 currentPage: 1,
                 pageSizes: [10, 20, 30, 40],
                 pageSize: 10,
-                total: 400,
+                total: '',
             }
         },
         created() {
-            this.queryDocumentAll(0);
+            this.queryDocumentAll("", this.currentPage, this.pageSize);
         },
         methods: {
             // 显示信息
@@ -55,14 +55,85 @@
                     type: type
                 });
             },
+            // 格式化数据
+            formatNumber: function(param) {
+                if (param < 10)
+                    return "0" + param;
+                return param;
+            },
+            // 格式化日期
+            formatDate: function(param) {
+                let date = new Date(param);
+                let fullYear = date.getFullYear();
+                let month = date.getMonth() + 1;
+                let day = date.getDate();
+                let hours = date.getHours();
+                let minutes = date.getMinutes();
+                let seconds = date.getSeconds();
+                return fullYear + "-" + this.formatNumber(month) + "-" + this.formatNumber(day) + " " +
+                    this.formatNumber(hours) +":"+ this.formatNumber(minutes) + ":" + this.formatNumber(seconds);
+            },
+            // 判断文件类型
+            getDocType: function(param) {
+                let type;
+                switch(param) {
+                    case 0:
+                        type = "图片";
+                        break;
+                    case 1:
+                        type = "文件";
+                        break;
+                    case 2:
+                        type = "视频";
+                        break;
+                    case 3:
+                        type = "其他";
+                        break;
+                }
+                return type;
+            },
+            // 判断文件夹是否删除
+            getDelFlag: function(param) {
+                let delFlag;
+                switch (param) {
+                    case 0:
+                        delFlag = "未删除";
+                        break;
+                    case 1:
+                        delFlag = "已删除";
+                        break;
+                }
+                return delFlag;
+            },
+            // 判断文件夹是物理删除还是逻辑删除
+            getDelFlagType: function(param) {
+                let delFlagType;
+                switch (param) {
+                    case 0:
+                        delFlagType = "逻辑删除（逻辑上删除）";
+                        break;
+                    case 1:
+                        delFlagType = "物理删除（彻底删除，不可恢复）";
+                        break;
+                }
+                return delFlagType;
+            },
             // 查询全部的目录文件夹
-            queryDocumentAll: function(param) {
+            queryDocumentAll: function(param, currentPage, pageSize) {
                 _self = this;
                 let url = "/api/upala/image/queryDocumentFolder";
-                let data = {"delFlag": param};
+                let data = {"delFlag": param, "index": currentPage, "limit": pageSize};
                 this.$axios.post(url, data).then((res) => {
                     if (res.data.status) {
-                        _self.documentSpaces = res.data.data;
+                        let dataArray = res.data.data;
+                        for (let i=0; i < dataArray.length; i++) {
+                            dataArray[i].createTime = _self.formatDate(dataArray[i].createTime);
+                            dataArray[i].type = _self.getDocType(dataArray[i].type);
+                            dataArray[i].delFlag = _self.getDelFlag(dataArray[i].delFlag);
+                            dataArray[i].delFlagType = _self.getDelFlagType(dataArray[i].delFlagType);
+                        }
+                        _self.documentSpaces = dataArray;
+                        _self.total = res.data.total;
                     }
                 }).catch((res) => {
                     _self.showInfo("请求失败：" + res, "error");
